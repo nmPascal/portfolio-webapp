e49d4923a2035d6e67617b6d1c93f24de1eed59d
PascalHcr <pascal.hector@netmedia.de>
2023-07-03 11:33:38 +0200

    get new hosted server

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -2,6 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://peridot-prairie-order.glitch.me/graphql",
+    // uri: "https://peridot-prairie-order.glitch.me/graphql",
+    uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
