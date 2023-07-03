8f59b33560d9806de0a992eef7b05075d18767b6
PascalHcr <pascal.hector@netmedia.de>
2023-07-03 11:34:13 +0200

    new api url

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -2,7 +2,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    // uri: "https://peridot-prairie-order.glitch.me/graphql",
-    uri: "http://localhost:4000/graphql",
+    uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
     cache: new InMemoryCache(),
 });
