36e18a825d641380ae2eba92ecbef5ae33ce1d30
PascalHcr <pascal.hector@netmedia.de>
2023-07-04 12:45:17 +0200

    style corrections

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -2,6 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
+    // uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
+    uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
