dfe686d802e7c0020858f8ca22bdddbec881ee6b
PascalHcr <pascal.hector@netmedia.de>
2023-07-26 11:52:52 +0200

    delete comments

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -2,8 +2,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    // uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
     uri: import.meta.env.VITE_GRAPHQL_SERVER_URL,
-    // uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
