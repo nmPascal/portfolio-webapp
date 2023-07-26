fbbf6d471bb908683f159183be7dc18ad7f13809
PascalHcr <pascal.hector@netmedia.de>
2023-07-26 11:45:54 +0200

    test .env netlify

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -2,7 +2,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
+    // uri: "https://portfolio-graphql-server-77f9c21f39c5.herokuapp.com/graphql",
+    uri: import.meta.env.VITE_GRAPHQL_SERVER_URL,
     // uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
