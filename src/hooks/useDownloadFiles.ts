3a086abb335a6c8bcba43c004033bc85c2442f36
PascalHcr <pascal.hector@netmedia.de>
2023-08-14 10:50:44 +0200

    Optimize all cv

--- src/hooks/useDownloadFiles.ts
+++ src/hooks/useDownloadFiles.ts
@@ -7,7 +7,7 @@ import { ECVLANG } from "../utils";
 export const useDownloadFiles = (): IDownloadFilesHookProps => {
     const downloadCV = (lang: ECVLANG) => {
         const { toLowerCase } = String.prototype;
-        const fileName = `PascalHector_${toLowerCase.call(lang).substring(0, 2)}_CV.pdf`;
+        const fileName = `PascalHector_${toLowerCase.call(lang).substring(0, 2)}_cv.pdf`;
         const filePath = `./assets/files/${fileName}`;
         const link = document.createElement("a");
 
